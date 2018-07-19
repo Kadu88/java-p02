0c5423fd56a906f88582061aea67ecdf814a5f62
Carlos Eduardo Barros <carlosuntr@gmail.com>
2018-07-18 23:26:13 -0300

    criação de métodos para testes simples

--- byteBank/src/br/com/carlosbarros/byteBank/controller/CriaConta.java
+++ byteBank/src/br/com/carlosbarros/byteBank/controller/CriaConta.java
@@ -1,8 +1,18 @@
 package br.com.carlosbarros.byteBank.controller;
 
+import br.com.carlosbarros.byteBank.model.Conta;
+
 public class CriaConta {
 
 	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.setSaldo(200);
+		primeiraConta.depositar(100);
 		
+		Conta segundaConta = new Conta();
+		segundaConta.setSaldo(50);
+		segundaConta.depositar(500);
+		segundaConta.retirar(400);
+		segundaConta.retirar(300);
 	}
 }
--- byteBank/src/br/com/carlosbarros/byteBank/model/Conta.java
+++ byteBank/src/br/com/carlosbarros/byteBank/model/Conta.java
@@ -1,9 +1,65 @@
 package br.com.carlosbarros.byteBank.model;
 
 public class Conta {
-	double saldo;
-	int agencia;
-	int numero;
-	String titular;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private String titular;
+	
+	
+	public void depositar(double valor) {
+		if(valor > 0) {
+			System.out.println("\nRealizando a deposito");
+			System.out.println("Saldo anterior: R$" + this.saldo);
+			System.out.println("Valor depositado: R$" + valor);
+			this.saldo += valor;
+			System.out.println("Saldo da conta corrente: R$" + this.saldo);
+		} else {
+			System.out.println("Não é possível depositar valores negativos, operação não realizada");
+		}
+	}
 
+	public void retirar(double valor) {
+		if (this.saldo >= valor) {
+			System.out.println("\nRealizando a retirada");
+			System.out.println("Saldo anterior: R$" + this.saldo);
+			System.out.println("Valor retirado: R$" + valor);
+			this.saldo -= valor;
+			System.out.println("Saldo da conta corrente: R$" + this.saldo);
+		} else {
+			System.out.println("\nRealizando a retirada");
+			System.out.println("Saldo anterior: R$" + this.saldo);
+			System.out.println("Valor retirado: R$" + valor);
+
+			System.out.println("\nSaldo insuficiente, não é possível retirar R$" +
+			valor + " de R$"+ this.saldo + ". Operação não realizada\n");
+		}
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	
 }
