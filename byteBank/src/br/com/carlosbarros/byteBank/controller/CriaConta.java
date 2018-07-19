8389df2ca4cbde21903effde7eaed70fbbc34fa6
Carlos Eduardo Barros <carlosuntr@gmail.com>
2018-07-18 22:26:36 -0300

    evolução, criação de pacotes + Classe Conta e CriaConta

---
+++ byteBank/src/br/com/carlosbarros/byteBank/controller/CriaConta.java
@@ -0,0 +1,8 @@
+package br.com.carlosbarros.byteBank.controller;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ byteBank/src/br/com/carlosbarros/byteBank/model/Conta.java
@@ -0,0 +1,9 @@
+package br.com.carlosbarros.byteBank.model;
+
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+
+}
